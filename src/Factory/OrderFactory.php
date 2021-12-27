55b554f235f7106ca708e01ce226ae55decf7d35
Gleb Kalinin <glebstuck@yandex.ru>
2021-12-27 17:41:47 +0500

    added OrderFactory

--- src/Factory/OrderFactory.php
+++ src/Factory/OrderFactory.php
@@ -2,7 +2,29 @@
 
 namespace App\Factory;
 
+use App\Entity\Order;
+use App\Entity\OrderItem;
+use App\Entity\Pizza;
+
 class OrderFactory
 {
+    public function create() : Order
+    {
+        $order = new Order();
+        $order
+            ->setStatus(Order::STATUS_CART)
+            ->setCreatedAt(new \DateTime())
+            ->setUpdatedAt(new \DateTime());
 
+        return $order;
+    }
+
+    public function createItem(Pizza $pizza): OrderItem
+    {
+        $item = new OrderItem();
+        $item->setProduct($pizza);
+        $item->setQuantity(1);
+
+        return $item;
+    }
 }
