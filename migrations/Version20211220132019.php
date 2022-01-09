5bdac1c2f7a124b0ae76455bbad509d877527ffb
Gleb Kalinin <glebstuck@yandex.ru>
2022-01-09 16:25:50 +0500

    fix in migration

--- migrations/Version20211220132019.php
+++ migrations/Version20211220132019.php
@@ -7,9 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
+
 final class Version20211220132019 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,7 +17,6 @@ final class Version20211220132019 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SEQUENCE ingredients_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE pizza_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE ingredients (id INT NOT NULL, title VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
@@ -28,8 +25,6 @@ final class Version20211220132019 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE ingredients_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE pizza_id_seq CASCADE');
         $this->addSql('DROP TABLE ingredients');
